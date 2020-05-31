1e32e1b42c2e823d93f73cf7ed68c62e01216818
Sebastien Denommee <mrsebastino@gmail.com>
2020-05-31 13:44:24 +0000

    fix equipment display - sync from edit to view

--- app.py
+++ app.py
@@ -36,11 +36,9 @@ Route to  exixting recipe and added recipe by user
 def our_recipes():
     all_recipes = mongo.db.recipes.find()
     all_categories = mongo.db.categories.find()
-    all_equipments = mongo.db.equipments.find()
 
     return render_template("our_recipes.html",
                            recipes=all_recipes,
-                           equipment=all_equipments,
                            categories=all_categories
                            )
 
@@ -54,11 +52,10 @@ def our_recipes():
 def add_recipes():
     all_recipes = mongo.db.recipes.find()
     all_categories = mongo.db.categories.find()
-    all_equipments = mongo.db.equipments.find()
+
     return render_template("add_recipes.html",
                            recipes=all_recipes,
                            categories=all_categories,
-                           equipment=all_equipments,
                            page_title="Add Recipe"
                            )
 
@@ -69,14 +66,12 @@ def add_recipes():
 """
 
 
-@app.route('/your_recipes/<recipes_id>')
-def your_recipes(recipes_id):
-    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
-    the_equipment = mongo.db.equipments.find_one()
+@app.route('/your_recipes/<db_id>')
+def your_recipes(db_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(db_id)})
 
     return render_template("your_recipes.html",
                            recipes=the_recipe,
-                           equipments=the_equipment,
                            page_title="Your Recipe"
                            )
 
@@ -90,18 +85,12 @@ def your_recipes(recipes_id):
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     recipes = mongo.db.recipes
-    equipments = mongo.db.equipments
     form_data = request.form.to_dict()
 
     ingredients_list = form_data["ingredients_name"].split("\n")
     instructions_list = form_data["instructions_name"].split("\n")
     equipments_list = form_data["equipment_name"].split("\n")
-    the_equipment = equipments.insert_one(
-        {
-            "equipment_name": equipments_list
 
-        }
-    )
     the_recipe = recipes.insert_one(
         {
             "category_name": form_data["category_name"],
@@ -111,13 +100,13 @@ def insert_recipe():
             "serve_name": form_data["serve_name"],
             "ingredients_name": ingredients_list,
             "instructions_name": instructions_list,
+            "equipment_name": equipments_list,
             "image_link": form_data["image_link"]
 
         }
     )
 
-    return redirect(url_for("your_recipes", recipes_id=the_recipe.inserted_id,
-                            equipments=equipments))
+    return redirect(url_for("your_recipes", db_id=the_recipe.inserted_id))
 
 
 """
@@ -125,35 +114,30 @@ def insert_recipe():
 """
 
 
-@app.route('/edit_recipe/<recipes_id>')
-def edit_recipe(recipes_id):
-    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
-    the_equipment = mongo.db.equipments.find_one()
+@app.route('/edit_recipe/<db_id>')
+def edit_recipe(db_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(db_id)})
     all_recipes = mongo.db.recipes.find()
-    all_equipments = mongo.db.equipments.find()
     all_categories = mongo.db.categories.find()
 
     return render_template("edit_recipes.html",
                            recipe=the_recipe,
-                           equipment=the_equipment,
                            recipes=all_recipes,
-                           equipments=all_equipments,
                            categories=all_categories,
                            page_title="Edit Recipe"
                            )
 
 
-@app.route('/update_recipe/<recipes_id>', methods=["POST"])
-def update_recipe(recipes_id):
+@app.route('/update_recipe/<db_id>', methods=["POST"])
+def update_recipe(db_id):
     recipes = mongo.db.recipes
-    equipments = mongo.db.equipments
     form_data = request.form.to_dict()
 
     ingredients_list = form_data["ingredients_name"].split("\n")
     instructions_list = form_data["instructions_name"].split("\n")
     equipments_list = form_data["equipments_name"].split("\n")
     recipes.update(
-        {"_id": ObjectId(recipes_id)},
+        {"_id": ObjectId(db_id)},
 
         {
             "category_name": form_data["category_name"],
@@ -168,21 +152,20 @@ def update_recipe(recipes_id):
 
         })
     return redirect(url_for("our_recipes",
-                            equipments=equipments,
-                            recipes_id=recipes_id
+                            db_id=db_id
                             ))
 
 
-@app.route('/delete_recipe/<recipes_id>')
-def delete_recipe(recipes_id):
-    mongo.db.recipes.remove({'_id': ObjectId(recipes_id)})
+@app.route('/delete_recipe/<db_id>')
+def delete_recipe(db_id):
+    mongo.db.recipes.remove({'_id': ObjectId(db_id)})
     return redirect(url_for('our_recipes'))
 
 
 @app.route('/equipments_list')
 def equipments_list():
-    all_equipments = mongo.db.equipments.find()
-    return render_template("equipment.html", equipments=all_equipments)
+    all_recipes = mongo.db.recipes.find()
+    return render_template("equipment.html", recipes=all_recipes)
 
 
 if __name__ == '__main__':
