4fd6f979f2915166cb407969b3cdd8da1092f9f0
Sebastien Denommee <mrsebastino@gmail.com>
2023-05-13 10:22:45 +0000

    removed import

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
-from jinja2 import escape
 
 """ DB URI for local workspace
 """
