867569ef8ba62edcb9e19d0389fad5da193a1390
Mrsebastino <mrsebastino@gmail.com>
2024-11-06 16:11:46 +0000

    Set debug to false

--- app.py
+++ app.py
@@ -209,4 +209,4 @@ def recipes_by_equipment(equipment_name):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
