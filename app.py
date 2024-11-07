d617cf3a6518db091d1a3b2ab265fb51db3b8302
Mrsebastino <mrsebastino@gmail.com>
2024-11-07 09:00:36 +0000

    Update to use PORT environment variable

--- app.py
+++ app.py
@@ -208,5 +208,5 @@ def recipes_by_equipment(equipment_name):
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
-            port=int(os.environ.get('PORT', 5000)),
+            port=int(os.environ.get('PORT', 8000)),
             debug=False)
